a41539e34f16b0ad24c79b8ee80e6ba3ea4e3353
praktiskt <mafur90@gmail.com>
2024-01-11 10:44:25 +0100

    align remaps, add lsp diagnostics

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,8 +1,8 @@
 local lsp = require('lsp-zero').preset({})
 
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- local cmp_action = require('lsp-zero').cmp_action()
+-- local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
 cmp.setup({
   preselect = 'item',
@@ -20,13 +20,16 @@ cmp.setup({
   }
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
 	lsp.default_keymaps({buffer = bufnr})
 	local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set('n', 'gne', function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set('n', 'ge', function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set('n', 'gpe', function() vim.diagnostic.goto_prev() end, opts)
 end)
 
 lsp.setup_servers({'tsserver', 'eslint'})
--- .config/nvim/lua/praktiskt/remap.lua
+++ .config/nvim/lua/praktiskt/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "gb", "<C-o>")
 vim.keymap.set("n", "gf", "<C-i>")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-f>", "/")
 
 -- Allow moving lines in visual mode
@@ -20,6 +20,7 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 -- In vertical edit mode, allow ctrl+c to paste
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -28,28 +29,28 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Multi-select
----- Rebind C-n to C-d
+-- Rebind C-n to C-d
 local binds = {}
 binds["Find Under"] = "<C-d>"
 binds["Find Subword Under"] = "<C-d>"
 vim.g.VM_maps = binds
 
 -- File tree config
-vim.keymap.set("n", "<leader>e", "<esc>:NvimTreeToggle<CR>")
-vim.keymap.set("v", "<leader>e", "<esc>:NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>te", "<esc>:NvimTreeToggle<CR>")
+vim.keymap.set("v", "<leader>te", "<esc>:NvimTreeToggle<CR>")
 
 -- Terminal shortcuts
-vim.keymap.set('n', '<leader>t', "<esc>:ToggleTerm<CR>")
-vim.keymap.set('v', '<leader>t', "<esc>:ToggleTerm<CR>")
+vim.keymap.set("n", "<leader>tt", "<esc>:ToggleTerm<CR>")
+vim.keymap.set("v", "<leader>tt", "<esc>:ToggleTerm<CR>")
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
