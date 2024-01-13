14d498f26ab024b87e57b080071e66112cb2f040
praktiskt <mafur90@gmail.com>
2024-01-13 16:15:58 +0100

    fix toggleterm hotkeys

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -4,10 +4,6 @@ vim.keymap.set("v", "<leader>tt", "<esc>:ToggleTerm<CR>")
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
   vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
@@ -26,7 +22,5 @@ local horizontal_terminal= Terminal:new({
 function _Toggle_floating_terminal() floating_terminal:toggle() end
 function _Toggle_horizontal_terminal() horizontal_terminal:toggle() end
 
-vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua _Toggle_floating_terminal()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("v", "<leader>tf", "<cmd>lua _Toggle_floating_terminal()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua _Toggle_horizontal_terminal()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("v", "<leader>tt", "<cmd>lua _Toggle_horizontal_terminal()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("x", "<leader>tf", "<cmd>lua _Toggle_floating_terminal()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("x", "<leader>tt", "<cmd>lua _Toggle_horizontal_terminal()<CR>", {noremap = true, silent = true})
