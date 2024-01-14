c379416fe204f14caee4bd180913850f8a6d6242
praktiskt <mafur90@gmail.com>
2024-01-14 14:41:16 +0100

    much nicer lsp print

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -11,25 +11,45 @@ cmp.setup({
   },
   sources = {
       {name = 'nvim_lsp'},
+      {name = 'nvim_lsp_signature_help'},
       {name = 'luasnip'}
   },
   mapping = {
     ['<CR>'] = cmp.mapping.confirm({select = false}),
     ['<Tab>'] = cmp.mapping.confirm({select = false}),
     ["<C-Space>"] = cmp.mapping.complete(),
-  }
+  },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+          local menu_icon ={
+              nvim_lsp = 'λ',
+              vsnip = '⋗',
+              buffer = 'Ω',
+              path = '🖫',
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+      end,
+  },
+
+
 })
 
 lsp.on_attach(function(_, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp.default_keymaps({buffer = bufnr})
-	local opts = {buffer = bufnr, remap = false}
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp.default_keymaps({buffer = bufnr})
+    local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', 'gne', function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n', 'ge', function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n', 'gpe', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'gne', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', 'ge', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', 'gpe', function() vim.diagnostic.goto_prev() end, opts)
 end)
 
 lsp.setup_servers({'tsserver', 'eslint'})
--- .config/nvim/after/plugin/mason.lua
+++ .config/nvim/after/plugin/mason.lua
@@ -1,6 +1,15 @@
 require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+        },
+    },
     log_level = vim.log.levels.DEBUG
 })
+
+require("mason-lspconfig").setup()
 
 
 
