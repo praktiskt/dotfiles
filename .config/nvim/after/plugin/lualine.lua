dc7604745b4bb03f60be105cb337ce93751db9fa
magnus <magnus.furugard@gmail.com>
2024-04-15 10:26:45 +0200

    style lualine

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,1 +1,14 @@
-require('lualine').setup()
+require('lualine').setup({
+    sections = {
+        lualine_c = {
+            { "filename", path = 1 }
+        }
+    },
+    options = {
+        icons_enabled = false,
+        globalstatus = false,
+        always_divide_middle = true,
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+    }
+}) 
