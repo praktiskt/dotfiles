6f08bfb2d0a38e981ba2c9a34fec880304a5f235
magnus <magnus.furugard@gmail.com>
2024-07-15 15:53:20 +0200

    remap add keys and formatting

--- .config/nvim/lua/praktiskt/remap.lua
+++ .config/nvim/lua/praktiskt/remap.lua
@@ -4,9 +4,12 @@ vim.keymap.set("n", "gf", "<C-i>")
 vim.keymap.set("n", "<C-f>", "/")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>i")
-vim.keymap.set({"n", "v"}, "<S-Up>", "<Up>")
-vim.keymap.set({"n", "v"}, "<S-Down>", "<Down>")
+vim.keymap.set({ "n", "v" }, "<S-Up>", "<Up>")
+vim.keymap.set({ "n", "v" }, "<S-Down>", "<Down>")
 vim.keymap.set("i", "<S-Tab>", "<C-\\><C-N><<<C-\\><C-N>^i")
+vim.keymap.set("i", "<A-BS>", "<Esc>ciw")
+vim.keymap.set("n", "<A-Right>", "e")
+vim.keymap.set("n", "<A-Left>", "b")
 
 -- Spelling shortcuts
 vim.keymap.set("n", "<leader>sn", "]s") -- next misspell
@@ -26,12 +29,12 @@ vim.keymap.set("n", "N", "nzzzv")
 vim.keymap.set("n", "<leader><leader>", "zz")
 
 -- Replace currently marked selection with buffer
-vim.keymap.set("n", "<leader>p", "\"_dP")
-vim.keymap.set("v", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>p", '"_dP')
+vim.keymap.set("v", "<leader>p", '"_dP')
 
 -- Allow yanking into system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
-vim.keymap.set({"n", "v"}, "<leader>Y", "\"+Y")
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>Y", '"+Y')
 
 -- In vertical edit mode, allow ctrl+c to paste
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -60,11 +63,10 @@ vim.keymap.set("n", "td", "<esc><cmd>Trouble diagnostics toggle<CR>")
 vim.keymap.set("n", "<leader>h", "<esc>:WhichKey<CR>")
 
 -- Tabby
-vim.g.tabby_trigger_mode = 'manual'
-vim.g.tabby_keybinding_accept = '<Tab>'
-vim.g.tabby_keybinding_trigger_or_dismiss = '<C-a>'
+vim.g.tabby_trigger_mode = "manual"
+vim.g.tabby_keybinding_accept = "<Tab>"
+vim.g.tabby_keybinding_trigger_or_dismiss = "<C-a>"
 
 -- LSP
-vim.keymap.set({"n", "v"}, '<leader>qf', ":lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
-vim.keymap.set({"n", "v"}, '<C-Space>', ":lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
-
+vim.keymap.set({ "n", "v" }, "<leader>qf", ":lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-Space>", ":lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
