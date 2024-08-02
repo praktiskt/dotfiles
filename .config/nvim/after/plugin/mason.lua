16f499274aa6b44b982ed82b11f4059b70a96046
magnus <magnus.furugard@gmail.com>
2024-08-02 22:27:04 +0200

    add and configure yamlls

--- .config/nvim/after/plugin/mason.lua
+++ .config/nvim/after/plugin/mason.lua
@@ -22,6 +22,7 @@ require("mason-lspconfig").setup({
 		"htmx",
 		"lua_ls",
 		"dockerls",
+		"yamlls",
 	},
 	handlers = {
 		lsp_zero.default_setup,
@@ -126,6 +127,27 @@ require("mason-lspconfig").setup({
 		end,
 		htmx = function()
 			lspconfig.htmx.setup({})
+		end,
+		yamlls = function()
+			lspconfig.yamlls.setup({
+				settings = {
+					yaml = {
+					    kubernetes = "*.yaml",
+                        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                        ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                        ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                        ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                        ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+                    },
+				},
+			})
 		end,
 	},
 })
