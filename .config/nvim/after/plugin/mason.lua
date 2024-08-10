86f3fa8c0ecd45d2b6e0f17d0747b03be9ae590c
magnus <magnus.furugard@gmail.com>
2024-08-10 23:44:18 +0200

    fix yamlls and helm_ls

--- .config/nvim/after/plugin/mason.lua
+++ .config/nvim/after/plugin/mason.lua
@@ -23,6 +23,7 @@ require("mason-lspconfig").setup({
 		"lua_ls",
 		"dockerls",
 		"yamlls",
+		"helm_ls",
 	},
 	handlers = {
 		lsp_zero.default_setup,
@@ -132,20 +133,45 @@ require("mason-lspconfig").setup({
 			lspconfig.yamlls.setup({
 				settings = {
 					yaml = {
-					    kubernetes = "*.yaml",
-                        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-                        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-                        ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-                        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-                        ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-                        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                        ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-                        ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-                        ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-                    },
+						kubernetes = "*.yaml",
+						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+						["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+						["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+						["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+						["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+						["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+						["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+						["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+						["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+					},
+				},
+			})
+		end,
+		helm_ls = function()
+			lspconfig.helm_ls.setup({
+				logLevel = "info",
+				valuesFiles = {
+					mainValuesFile = "values.yaml",
+					lintOverlayValuesFile = "values.lint.yaml",
+					additionalValuesFilesGlobPattern = "values*.yaml",
+				},
+				yamlls = {
+					enabled = false,
+					enabledForFilesGlob = "*.{yaml,yml}",
+					diagnosticsLimit = 50,
+					showDiagnosticsDirectly = false,
+					path = "yaml-language-server",
+					config = {
+						schemas = {
+							kubernetes = "templates/**",
+						},
+						completion = true,
+						hover = true,
+						-- any other config from https://github.com/redhat-developer/yaml-language-server#language-server-settings
+					},
 				},
 			})
 		end,
