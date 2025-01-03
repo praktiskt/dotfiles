aee84024d539d95f82a29eb43eef69045f460795
magnus <magnus.furugard@gmail.com>
2025-01-03 12:51:19 +0100

    feat: update plugin versions and groq provider configuration in lazy.lua

--- .config/nvim/lua/praktiskt/lazy.lua
+++ .config/nvim/lua/praktiskt/lazy.lua
@@ -272,35 +272,10 @@ require("lazy").setup({
 				provider = "groq",
 				vendors = {
 					groq = {
-						endpoint = "https://api.groq.com/openai/v1/chat/completions",
-						model = "llama-3.1-70b-versatile",
+						__inherited_from = "openai",
 						api_key_name = "GROQ_API_KEY",
-						parse_curl_args = function(opts, code_opts)
-							return {
-								url = opts.endpoint,
-								headers = {
-									["Accept"] = "application/json",
-									["Content-Type"] = "application/json",
-									["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
-								},
-								body = {
-									model = opts.model,
-									messages = { -- you can make your own message, but this is very advanced
-										{ role = "system", content = code_opts.system_prompt },
-										{
-											role = "user",
-											content = require("avante.providers.openai").get_user_message(code_opts),
-										},
-									},
-									temperature = 0,
-									max_tokens = 4096,
-									stream = true, -- this will be set by default.
-								},
-							}
-						end,
-						parse_response_data = function(data_stream, event_state, opts)
-							require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-						end,
+						endpoint = "https://api.groq.com/openai/v1/",
+						model = "llama-3.3-70b-versatile",
 					},
 				},
 			},
