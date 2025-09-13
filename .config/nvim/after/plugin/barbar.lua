89105800375278cb90bbc837fa2ce9f36894e745
magnus <magnus.furugard@gmail.com>
2025-09-13 11:09:35 +0200

    feat(nvim): add buffer move keymaps for barbar plugin

--- .config/nvim/after/plugin/barbar.lua
+++ .config/nvim/after/plugin/barbar.lua
@@ -5,3 +5,5 @@ require("barbar").setup({
 })
 vim.keymap.set({ "i", "n", "v" }, "<C-A-left>", "<Cmd>BufferPrevious<CR>", {})
 vim.keymap.set({ "i", "n", "v" }, "<C-A-right>", "<Cmd>BufferNext<CR>", {})
+vim.keymap.set({ "i", "n", "v" }, "<S-A-left>", "<Cmd>BufferMovePrevious<CR>", {})
+vim.keymap.set({ "i", "n", "v" }, "<S-A-right>", "<Cmd>BufferMoveNext<CR>", {})
