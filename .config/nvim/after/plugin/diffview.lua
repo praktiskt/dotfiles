8aba9b3a1630092369cdc195e21f67df740f51d2
magnus <magnus.furugard@gmail.com>
2025-09-14 11:21:56 +0200

    feat(nvim): enhance diffview setup with custom keymaps and toggle functionality

--- .config/nvim/after/plugin/diffview.lua
+++ .config/nvim/after/plugin/diffview.lua
@@ -1,3 +1,22 @@
-require("diffview").setup()
-vim.keymap.set("n", "<leader>do", "<Cmd>DiffviewOpen<CR>")
-vim.keymap.set("n", "<leader>dq", "<Cmd>DiffviewClose<CR>")
+local actions = require("diffview.actions")
+require("diffview").setup({
+	keymaps = {
+		file_panel = {
+			["<A-CR>"] = actions.goto_file_tab,
+		},
+	},
+})
+
+local diffview = require("diffview.lib")
+local function diffview_toggle()
+	local view = diffview.get_current_view()
+	if view then
+		vim.cmd("DiffviewClose")
+	else
+		vim.cmd("DiffviewOpen")
+	end
+end
+
+vim.api.nvim_create_user_command("DiffviewToggle", diffview_toggle, {})
+vim.keymap.set("n", "<leader>tg", diffview_toggle, { desc = "Toggle Diffview" })
+vim.keymap.set("n", "tg", diffview_toggle, { desc = "Toggle Diffview" })
