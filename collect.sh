cad2be436145d183b6f42981aa23026ccef71b40
magnus <magnus.furugard@gmail.com>
2025-09-14 20:07:50 +0200

    feat(collect.sh): add wezterm configuration collection to script

--- collect.sh
+++ collect.sh
@@ -7,69 +7,73 @@ set -e
 # * All files collected shall be placed in a folder identical to the function name
 
 collector() {
-	P=$1
-	mkdir -p $P
-	FILES="${@:2}"
-	for FILE in $FILES; do
-		cp ~/$P/$FILE $P/$FILE
-	done
+    P=$1
+    mkdir -p $P
+    FILES="${@:2}"
+    for FILE in $FILES; do
+        cp ~/$P/$FILE $P/$FILE
+    done
 }
 
 alacritty() {
-	collector .config/alacritty alacritty.yml
+    collector .config/alacritty alacritty.yml
 }
 
 bin() {
-	collector bin groq
-	collector bin commitmsg
+    collector bin groq
+    collector bin commitmsg
 }
 
 zed() {
-	collector .config/zed $(ls ~/.config/zed | grep json | xargs)
-	collector .config/zed/snippets $(ls ~/.config/zed/snippets | xargs)
+    collector .config/zed $(ls ~/.config/zed | grep json | xargs)
+    collector .config/zed/snippets $(ls ~/.config/zed/snippets | xargs)
 }
 
 regolith() {
-	collector .config/regolith3 Xresources
-	# collector .config/i3lock.sh Xresources
+    collector .config/regolith3 Xresources
+    # collector .config/i3lock.sh Xresources
 }
 
 fusuma() {
-	collector .config/fusuma config.yaml
+    collector .config/fusuma config.yaml
 }
 
 ohmyzh() {
-	collector .oh-my-zsh/themes gitster.zsh-theme
+    collector .oh-my-zsh/themes gitster.zsh-theme
 }
 
 mpv() {
-	collector .config/mpv mpv.conf
+    collector .config/mpv mpv.conf
 }
 
 neovide() {
-	collector .config/neovide config.toml
+    collector .config/neovide config.toml
+}
+
+wezterm() {
+    collector .config/wezterm wezterm.lua
 }
 
 nvim() {
-	LUAS=$(find ~/.config/nvim -name "*.lua" | sed -E 's/^.*nvim\///g' | xargs)
-	SNIPPETS=$(find ~/.config/nvim -name "*.snippets" | sed -E 's/^.*nvim\///g' | xargs)
-	SPELLING=$(find ~/.config/nvim -name "*.add" | sed -E 's/^.*nvim\///g' | xargs)
-	SPELLING_SPL=$(find ~/.config/nvim -name "*.add.spl" | sed -E 's/^.*nvim\///g' | xargs)
-	FILES=$(echo "$LUAS $SNIPPETS $SPELLING $SPELLING_SPL")
-	mkdir -p .config/nvim/after/plugin/lsp
-	mkdir -p .config/nvim/lua/praktiskt
-	mkdir -p .config/nvim/snippets .config/nvim/spell
-	for FILE in $FILES; do
-		if [[ $(echo $FILE | grep 'packer_compiled.lua') ]]; then
-			continue
-		fi
-		collector .config/nvim $FILE
-	done
+    LUAS=$(find ~/.config/nvim -name "*.lua" | sed -E 's/^.*nvim\///g' | xargs)
+    SNIPPETS=$(find ~/.config/nvim -name "*.snippets" | sed -E 's/^.*nvim\///g' | xargs)
+    SPELLING=$(find ~/.config/nvim -name "*.add" | sed -E 's/^.*nvim\///g' | xargs)
+    SPELLING_SPL=$(find ~/.config/nvim -name "*.add.spl" | sed -E 's/^.*nvim\///g' | xargs)
+    FILES=$(echo "$LUAS $SNIPPETS $SPELLING $SPELLING_SPL")
+    mkdir -p .config/nvim/after/plugin/lsp
+    mkdir -p .config/nvim/lua/praktiskt
+    mkdir -p .config/nvim/snippets .config/nvim/spell
+    for FILE in $FILES; do
+        if [[ $(echo $FILE | grep 'packer_compiled.lua') ]]; then
+            continue
+        fi
+        collector .config/nvim $FILE
+    done
 }
 
 aliases() {
-	# Add these to your current shell.
-	cat <<EOF >aliases
+    # Add these to your current shell.
+    cat <<EOF >aliases
 # Aliases from praktiskt/dotfiles
 alias vim="nvim"
 alias open="xdg-open"
@@ -89,15 +93,16 @@ EOF
 }
 
 main() {
-	nvim
-	bin
-	# alacritty
-	regolith
-	fusuma
-	ohmyzh
-	mpv
-	aliases
-	zed
+    nvim
+    bin
+    # alacritty
+    regolith
+    fusuma
+    ohmyzh
+    mpv
+    aliases
+    zed
+    wezterm
 }
 
 main $@
