6982c8db53d33a38f9917c2b76e967d8deceed0e
magnus <magnus.furugard@gmail.com>
2025-09-14 16:20:41 +0200

    feat(nvim): add barbar and multicursor plugin configurations and keymaps

--- .config/nvim/after/plugin/barbar.lua
+++ .config/nvim/after/plugin/barbar.lua
@@ -1,9 +1,15 @@
 require("barbar").setup({
+	animation = false,
+	auto_hide = true,
 	icons = {
 		modified = { button = "*" },
 	},
 })
 vim.keymap.set({ "i", "n", "v" }, "<C-A-left>", "<Cmd>BufferPrevious<CR>", {})
 vim.keymap.set({ "i", "n", "v" }, "<C-A-right>", "<Cmd>BufferNext<CR>", {})
+vim.keymap.set({ "i", "n", "v" }, "<M-Tab>", "<Cmd>BufferNext<CR>", {})
+vim.keymap.set({ "i", "n", "v" }, "<M->>", "<Cmd>BufferNext<CR>", {})
+vim.keymap.set({ "i", "n", "v" }, "<M-<>", "<Cmd>BufferPrevious<CR>", {})
+
 vim.keymap.set({ "i", "n", "v" }, "<S-A-left>", "<Cmd>BufferMovePrevious<CR>", {})
 vim.keymap.set({ "i", "n", "v" }, "<S-A-right>", "<Cmd>BufferMoveNext<CR>", {})
--- .config/nvim/after/plugin/multicursor.lua
+++ .config/nvim/after/plugin/multicursor.lua
@@ -1,14 +1,14 @@
 local mc = require("multicursor-nvim")
 mc.setup()
-vim.keymap.set({"n", "v"}, "<C-d>", function() mc.matchAddCursor(1) end)
-mc.addKeymapLayer(function(layerSet)
-    layerSet({"n", "v"}, "<esc>", function()
-        if not mc.cursorsEnabled() then
-            mc.enableCursors()
-        else
-            mc.clearCursors()
-        end
-    end)
+vim.keymap.set({ "n", "v" }, "<C-d>", function()
+	mc.matchAddCursor(1)
 end)
-
-
+mc.addKeymapLayer(function(layerSet)
+	layerSet({ "n", "v" }, "<esc>", function()
+		if not mc.cursorsEnabled() then
+			mc.enableCursors()
+		else
+			mc.clearCursors()
+		end
+	end)
+end)
