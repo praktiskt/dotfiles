ea3f30f590fef9d93028e3f9ae9ea72fa6f1998b
magnus <magnus.furugard@gmail.com>
2025-09-14 10:59:05 +0200

    feat(telescope): add lsp references and live grep mappings and config

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,14 +4,14 @@ vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fw", function()
 	builtin.grep_string()
 end)
-vim.keymap.set("n", "<leader>ff", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references)
+vim.keymap.set("n", "<leader>ff", builtin.live_grep)
 
 require("telescope").setup({
 	defaults = {
 		file_ignore_patterns = {
 			"target/debug",
 		},
-	}
+	},
 })
