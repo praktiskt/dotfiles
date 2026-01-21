340761152e4d149140ab91f49a1a455388c6de6a
magnus <magnus.furugard@gmail.com>
2026-01-21 13:05:02 +0100

    feat(treesitter): add install_dir, parsers list, autocmd and custom textobject keymaps

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,72 +1,85 @@
 require("nvim-treesitter.install").prefer_git = true
 require("nvim-treesitter.config").setup({
-	ensure_installed = {
-		"bash",
-		"c",
-		"cmake",
-		"elixir",
-		"erlang",
-		"git_config",
-		"git_rebase",
-		"gitattributes",
-		"gitcommit",
-		"gitignore",
-		"go",
-		"gomod",
-		"gosum",
-		"gotmpl",
-		"gpg",
-		"helm",
-		"html",
-		"java",
-		"javascript",
-		"jq",
-		"json",
-		"json5",
-		"kotlin",
-		"lua",
-		"luadoc",
-		"make",
-		"markdown",
-		"markdown_inline",
-		"nginx",
-		"php",
-		"python",
-		"r",
-		"regex",
-		"rust",
-		"scala",
-		"sql",
-		"ssh_config",
-		"svelte",
-		"terraform",
-		"toml",
-		"toml",
-		"vim",
-		"vimdoc",
-		"vue",
-		"yaml",
-	},
-	sync_install = false,
-	auto_install = false,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	endwise = {
-		enable = true,
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true,
-			keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-			},
-			include_surrounding_whitespace = true,
-		},
+	install_dir = vim.fn.stdpath("data") .. "/site",
+})
+
+local parsers = {
+	"bash",
+	"c",
+	"cmake",
+	"elixir",
+	"erlang",
+	"git_config",
+	"git_rebase",
+	"gitattributes",
+	"gitcommit",
+	"gitignore",
+	"go",
+	"gomod",
+	"gosum",
+	"gotmpl",
+	"gpg",
+	"helm",
+	"html",
+	"java",
+	"javascript",
+	"jq",
+	"json",
+	"json5",
+	"kotlin",
+	"lua",
+	"luadoc",
+	"make",
+	"markdown",
+	"markdown_inline",
+	"nginx",
+	"php",
+	"python",
+	"r",
+	"regex",
+	"rust",
+	"scala",
+	"sql",
+	"ssh_config",
+	"svelte",
+	"terraform",
+	"toml",
+	"vim",
+	"vimdoc",
+	"vue",
+	"yaml",
+}
+
+-- require("nvim-treesitter").install(parsers)
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = vim.tbl_keys(require("nvim-treesitter.parsers")),
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
+
+require("nvim-treesitter-textobjects").setup({
+	select = {
+		lookahead = true,
+		include_surrounding_whitespace = false,
 	},
 })
+
+vim.keymap.set({ "x", "o" }, "af", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "if", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ac", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ic", function()
+	require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+end)
+
+require("treesitter-context").setup({})
